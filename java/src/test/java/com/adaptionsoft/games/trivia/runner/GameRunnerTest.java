88df8804864ff80b7593bdd1c7c9df44a27b29df
Vytenis Bivainis <monrealis@gmail.com>
2015-02-28 11:46:35 +0200

    Improve GameRunnerTest.

--- java/src/test/java/com/adaptionsoft/games/trivia/runner/GameRunnerTest.java
+++ java/src/test/java/com/adaptionsoft/games/trivia/runner/GameRunnerTest.java
@@ -1,12 +1,13 @@
 package com.adaptionsoft.games.trivia.runner;
 
+import static java.lang.String.join;
+import static java.nio.file.Files.readAllLines;
+import static java.nio.file.Files.write;
 import static org.junit.Assert.assertEquals;
 
 import java.io.ByteArrayOutputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Random;
@@ -15,34 +16,46 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class GameRunnerTest {
-	
 	private static final Path masterFile = Paths.get(".", "master.txt");
-	
 
-	//@BeforeClass
-	public static void createMAsterFile() throws IOException {
-		Files.write(masterFile, playLotsOfGame().getBytes());
+	@BeforeClass
+	public static void createMasterFile() throws IOException {
+		if (!shouldGenerateMasterFile())
+			return;
+		write(masterFile, playLotsOfGames().getBytes());
 	}
-	
-	public static String playLotsOfGame() {
-		ByteArrayOutputStream stream = new  ByteArrayOutputStream();
+
+	private static boolean shouldGenerateMasterFile() {
+		return false;
+	}
+
+	public static String playLotsOfGames() {
+		return captureSystemOut(() -> playLotsOfGames(1000));
+	}
+
+	private static String captureSystemOut(Runnable runnable) {
+		PrintStream oldOut = System.out;
+		ByteArrayOutputStream stream = new ByteArrayOutputStream();
 		System.setOut(new PrintStream(stream));
-		
-		for (int i = 1; i < 1000; i++) {
-			GameRunner.play(new Random(i));
-			System.out.println("==================================");
-		}
-		
+		runnable.run();
+		System.setOut(oldOut);
 		return stream.toString();
 	}
 
-	@Test
-	public void test() throws IOException {
-		String expected = String.join("\n", Files.readAllLines(masterFile)) + "\n";
-		String actual = playLotsOfGame();
-		
-		assertEquals(expected, actual);
-		
+	private static void playLotsOfGames(int n) {
+		for (int i = 1; i < n; i++)
+			playOneGame(i);
 	}
 
+	private static void playOneGame(int randomSeed) {
+		GameRunner.play(new Random(randomSeed));
+		System.out.println("==================================");
+	}
+
+	@Test
+	public void outputUnchangedFromPreviousVersion() throws IOException {
+		String expected = join("\n", readAllLines(masterFile)) + "\n";
+		String actual = playLotsOfGames();
+		assertEquals(expected, actual);
+	}
 }
