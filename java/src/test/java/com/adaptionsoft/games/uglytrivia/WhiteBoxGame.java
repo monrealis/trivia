2dc8a96610a6fd7208b843a415d143884b325f2e
Vytenis Bivainis <monrealis@gmail.com>
2015-02-28 13:10:02 +0200

    Add println capturing.

--- java/src/test/java/com/adaptionsoft/games/uglytrivia/WhiteBoxGame.java
+++ java/src/test/java/com/adaptionsoft/games/uglytrivia/WhiteBoxGame.java
@@ -1,5 +1,24 @@
 package com.adaptionsoft.games.uglytrivia;
 
-public class WhiteBoxGame extends Game {
+import java.util.ArrayList;
+import java.util.List;
 
+public class WhiteBoxGame extends Game {
+	private List<String> lines = new ArrayList<>();
+
+	@Override
+	protected void println(Object text) {
+		lines.add(text.toString());
+		super.println(text);
+	}
+
+	public String getJoinedLines() {
+		if (lines.isEmpty())
+			return "";
+		return String.join("\n", lines) + "\n";
+	}
+	
+	public void reset() {
+		lines.clear();
+	}
 }
